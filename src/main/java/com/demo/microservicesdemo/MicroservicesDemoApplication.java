255bfac9edcfe18e85b18aba6fe49306ceb2a103
Mihail Slavov <mihail_slavov@icloud.com>
2024-02-26 17:04:33 +0200

    test v1

---
+++ src/main/java/com/demo/microservicesdemo/MicroservicesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.microservicesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/microservicesdemo/MicroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.microservicesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
